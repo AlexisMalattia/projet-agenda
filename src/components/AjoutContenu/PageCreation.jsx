62240202e08deac0955933f9ed7fca7bdb52a417
AlexisMalattia <alexis.malattia06@gmail.com>
2023-03-02 15:39:29 +0100

    yo

--- src/components/AjoutContenu/PageCreation.jsx
+++ src/components/AjoutContenu/PageCreation.jsx
@@ -1,13 +1,37 @@
 import {useNavigate} from "react-router-dom";
+import '../../styles/PageCreation.scss'
+import { TextInput, Checkbox, Button, Group, Box, Textarea} from '@mantine/core';
+import { useForm} from "@mantine/form";
 
 function PageCreation(){
     const navigate = useNavigate();
     function retourAccueil(){
         navigate('/');
     }
+    const form = useForm({
+        initialValues: {
+            nomDuDevoir: '',
+            dateDeRendu: '',
+            description: '',
+            module: ''
+        }
+    });
     return (
-        <div>
-            <button onClick={retourAccueil}> Retour accueil </button>
+        <div className='CreationMainDiv'>
+            <div>
+                <button onClick={retourAccueil}> Retour accueil </button>
+            </div>
+            <div className='EcranCreation'>
+                <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                    <div className="formHeader">
+                        <TextInput withAsterisk label="Nom du devoir" name="nomDuDevoir" required className="inputForm"/>
+                        <TextInput withAsterisk label="Date de rendu" name="dateDeRendu" required className="inputForm"/>
+                    </div>
+                    <Textarea rows={5} label={'Description'} name="description" required className="descriptionInputForm" styles={{ field: { height: '20em' } }}/>
+                    <TextInput withAsterisk label="Module" name="module" required />
+
+                </form>
+            </div>
         </div>
     )
 }
